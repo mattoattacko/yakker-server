e45c55203ffc0c2d9e4435033e75a8eec22ec624
Matto <matt.mcquain@gmail.com>
2019-02-16 20:27:01 -0800

    re-doing the server

---
+++ header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Welcome to the N7 Seattle Seahawks Chat Room</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
